641372f887c8ac47bdf342e62d5c8d856838b13f
Wejdan Hakami <wejdan.hakami.ha@gmail.com>
2026-07-03 11:50:49 +0300

    Add Java program to calculate product of three numbers
    This Java program reads three double numbers from the user and calculates their product.

---
+++ product-of-three-numbers/Main.java
@@ -0,0 +1,15 @@
+# Product of Three Numbers
+
+## Description
+This is a simple Java program that reads three double numbers from the user and calculates their product.
+
+## Concepts
+- Basic input/output
+- Variables
+- Arithmetic operations
+
+## Input
+Three double values from the user.
+
+## Output
+The product of the three numbers.
